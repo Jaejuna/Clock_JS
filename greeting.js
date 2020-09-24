fa2ffd13823b3404efad12fcb04445000d245626
Jaejuna <anayana9988@gmail.com>
2020-09-24 21:50:56 +0900

    JS edited

--- greeting.js
+++ greeting.js
@@ -1,11 +1,11 @@
-const form = document.querySelector(".js-form");
-const greeting = document.querySelector(".js-greeting");
-const input = form.querySelectorAll("input");
+const form = document.querySelector(".js-form"),
+input = form.querySelector("input"),
+greeting = document.querySelector(".js-greeting");
 
-const USER_LS = "currentUser";
-const SHOWING_CN = "showing";
+const USER_LS = "currentUser",
+SHOWING_CN = "showing";
 
-function saveName(text){
+function saveName(text) {
     localStorage.setItem(USER_LS, text);
 }
 
@@ -21,24 +21,24 @@ function askForName() {
     form.addEventListener("submit", handleSubmit);
 }
 
-function paintGreeting(text){
+function paintGreeting(text) {
     form.classList.remove(SHOWING_CN);
     greeting.classList.add(SHOWING_CN);
     greeting.innerText = `Hello ${text}`;
 }
 
-function loadname(){
-    const currentUser = localStorage.getItem(USER_LS);
-    if(currentUser = null){
-        askForName();
-    }
-    else{
-        paintGreeting(currentUser);
-    }
+function loadName() {
+const currentUser = localStorage.getItem(USER_LS);
+    if (currentUser === null) {
+    askForName();
+} 
+    else {
+    paintGreeting(currentUser);
+}
 }
 
-function init(){
-    loadname(); 
+function init() {
+loadName();
 }
 
 init();
