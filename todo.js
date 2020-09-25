21d38e98d5c685cfa72d44da32adb549b21b33db
Jaejuna <anayana9988@gmail.com>
2020-09-26 02:34:45 +0900

    todo JS_2

--- todo.js
+++ todo.js
@@ -4,22 +4,41 @@ const todoList = document.querySelector(".js-todoList");
 
 const TODOS_LS = 'todos';
 
-function loadToDos(){
-    const toDos = localStorage.getItem(TODOS_LS);
-    if (toDos !== null){
+const toDos = [];
 
+function saveToDos(){
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+
+function loadToDos(){
+    const loadedToDos = localStorage.getItem(TODOS_LS);
+    if (loadedToDos !== null){
+        const parsedToDos = JSON.parse(loadedToDos);
+        parsedToDos.forEach(function(toDo){
+            paintToDo(toDo.text);
+        });
     }
 }
 
 function paintToDo(text){
     const li = document.createElement("li");
     const delBtn = document.createElement("button");
-    delBtn.innerText = "❌";
     const span = document.createElement("span");
+    const newId = toDos.length + 1;
+
+    delBtn.innerText = "❌";
     span.innerText = text;
     li.appendChild(delBtn);
     li.appendChild(span);
+    li.id = newId;
+
     todoList.appendChild(li);
+    const toDoObj = {
+        text : text,
+        id: newId
+    };
+    toDos.push(toDoObj);
+    saveToDos();
 }
 
 function handleSubmit(event){
