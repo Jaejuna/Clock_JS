a9d48ddad36486c79b13c40b4b21823a9109da7a
Jaejuna <anayana9988@gmail.com>
2020-09-28 01:59:59 +0900

    added bgJS & weatherJS

--- JS/todo.js
+++ JS/todo.js
@@ -4,7 +4,18 @@ const todoList = document.querySelector(".js-todoList");
 
 const TODOS_LS = 'todos';
 
-const toDos = [];
+let toDos = [];
+
+function deleteToDo(event){
+    const btn = event.target;
+    const li = btn.parentNode;
+    todoList.removeChild(li);
+    const cleanToDos = toDos.filter(function(toDo){
+        return toDo.id !== parseInt(li.id);
+    });
+    toDos = cleanToDos;
+    saveToDos();
+}
 
 function saveToDos(){
     localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
@@ -27,6 +38,7 @@ function paintToDo(text){
     const newId = toDos.length + 1;
 
     delBtn.innerText = "❌";
+    delBtn.addEventListener("click", deleteToDo);
     span.innerText = text;
     li.appendChild(delBtn);
     li.appendChild(span);
---
+++ JS/weather.js
@@ -0,0 +1,57 @@
+const weather = document.querySelector(".js-weather");
+const refreshBtn = document.querySelector(".refreshs");
+const API_KEY = "a559c2cf5c1c25801a4701b1f8384b5c";
+const COORDS = "coords";
+
+function getweather(lat, lng) {
+    fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+    )
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (json) {
+        const temperature = json.main.temp;
+        const place = json.name;
+        const tdweather = json.weather[0].main;
+        weather.innerText = `${temperature}°C  ${tdweather} @ ${place}`;
+    });
+}
+
+function saveCoords(coordsObj) {
+    localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSuccess(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const coordsObj = {
+    latitude,
+    longitude,
+    };
+    saveCoords(coordsObj);
+    getweather(latitude, longitude);
+}
+
+function handleGeoError() {
+    console.log("can't access geo location");
+}
+function askForCoords() {
+    navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError);
+}
+
+function loadCoords() {
+    const loadedCoords = localStorage.getItem(COORDS);
+    if (loadedCoords === null) {
+    askForCoords();
+    } else {
+    const parseCoords = JSON.parse(loadedCoords);
+    getweather(parseCoords.latitude, parseCoords.longitude);
+    }
+}
+
+function init() {
+    loadCoords();
+}
+
+init();
